5c7a36772fd4e30b8d479e2067b99f5c8173bdda
brendanwang <brendanmwang@gmail.com>
2026-09-13 15:50:24 -0700

    Initial Commit

---
+++ TransferBar/TransferBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransferBarApp.swift
+//  TransferBar
+//
+//  Created by Brendan Wang on 9/13/26.
+//
+
+import SwiftUI
+
+@main
+struct TransferBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
